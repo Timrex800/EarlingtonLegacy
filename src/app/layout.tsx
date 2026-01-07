c6c5c127264b1da85d1e106ad826f3d1627b8f68
Timothy Padayachee <timrex800@gmail.com>
2026-01-08 01:55:52 +0200

    Add Google Tag Manager and Google Analytics (gtag.js) snippets to site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,15 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MJHTWL9F');`,
+          }}
+        />
         <link rel="canonical" href="https://www.earlingtonlegacy.org.za/" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
